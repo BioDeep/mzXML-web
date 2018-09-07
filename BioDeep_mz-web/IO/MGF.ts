71f9bfbc0aced0a89dffbac117907f7389e21de7
xieguigang <xie.guigang@gcmodeller.org>
2018-09-07 11:06:40 +0800

    add mgf fields

--- BioDeep_mz-web/IO/MGF.ts
+++ BioDeep_mz-web/IO/MGF.ts
@@ -2,17 +2,54 @@
 
 namespace BioDeep.IO {
 
+    const mgfBeginIons: string = "BEGIN IONS"
+    const mgfEndIons: string = "END IONS";
+
     /**
+     * mascot generic format files
+     * 
+     * > http://fiehnlab.ucdavis.edu/projects/LipidBlast/mgf-files
+     * 
      * 在这个模块之中解析mgf格式的质谱图数据
     */
     export class mgf extends IEnumerator<BioDeep.Models.mzInto> {
+
+        /**
+         * PEPMASS
+        */
+        public precursor_mass: number;
+        /**
+         * CHARGE
+        */
+        public charge: number;
+        /**
+         * RTINSECONDS
+        */
+        public rt: number;
+        /**
+         * TITLE
+        */
+        public title: string;
 
         public get mzInto(): BioDeep.Models.mzInto[] {
             return [...this.sequence];
         };
 
-        public static Parse(text: string): mgf {
+        public static Parse(text: string): IEnumerator<mgf> {
+            var lines: string[] = text.split("\n");
+            var list: mgf[] = [];
 
+
+
+            return new IEnumerator<mgf>(list);
+        }
+
+        public static IonParse(data: string[]): mgf {
+
+        }
+
+        public toString(): string {
+            return this.title;
         }
     }
 }
