d2ac614ef7b74f646321d76b935cc15091a67955
xieguigang <xie.guigang@gcmodeller.org>
2018-09-14 09:57:03 +0800

    add code comments

--- BioDeep_mz-web/Models/mz.ts
+++ BioDeep_mz-web/Models/mz.ts
@@ -3,12 +3,18 @@
     export class mzInto {
 
         public id: string;
+
         /**
          * m/z
+         * 
+         * X 坐标轴数据
         */
         public mz: number;
         /**
          * intensity
+         * 
+         * 进行绘图操作的时候所需要的Y坐标轴数据，以及鼠标提示
+         * 显示所需要用到的原始的质谱信号强度数据
         */
         public into: number;
 
