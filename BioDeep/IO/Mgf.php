230ae3db8103337be57a96314a4e101eb6c2f1f2
xieguigang <xie.guigang@gcmodeller.org>
2018-09-17 11:28:27 +0800

    add mgf file handler

---
+++ BioDeep/IO/Mgf.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace BioDeep\IO {
+
+    class Mgf {
+
+        /**
+         * 生成一个mgf对象数据
+         * 
+         * @param array $meta 在这个字典数组之中应该至少要包括有字段：``mz``，``rt``。
+         * @param array $ms2 二级质谱数据矩阵
+        */
+        public static function CreateDocument($meta, $ms2) {
+
+        }
+    }
+}
