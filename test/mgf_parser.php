52956efbed7b885d1bf81299ef745879f15cdbb7
xieguigang <xie.guigang@gcmodeller.org>
2018-10-12 10:40:17 +0800

    add file dump test

--- test/mgf_parser.php
+++ test/mgf_parser.php
@@ -6,8 +6,10 @@ include __DIR__ . "/../../dotnet/package.php";
 include __DIR__ . "/../autoload.php";
 
 use BioDeep\IO\MgfIon as Reader;
+use BioDeep\IO\MgfWriter as Writer;
 
 foreach (Reader::PopulateIons(__DIR__ . "/GABA.mgf") as $ion) {
-    echo var_dump($ion);
+    $doc = Writer::CreateDocument($ion->precursor, $ion->MzInto);
+    echo $doc;
     echo "\n\n\n\n";
 }
