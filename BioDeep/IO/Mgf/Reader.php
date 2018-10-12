d7e23b2be0d0e567074cd494cf441f75b2f9b72e
xieguigang <xie.guigang@gcmodeller.org>
2018-10-12 09:04:28 +0800

    mgf text file parser

--- BioDeep/IO/Mgf/Reader.php
+++ BioDeep/IO/Mgf/Reader.php
@@ -2,7 +2,66 @@
 
 namespace BioDeep\IO {
 
+    Imports("Microsoft.VisualBasic.FileIO.FileSystem");
+
     class MgfReader {
         
+        /**
+         * @param string $file File path or file content.
+        */
+        public static function PopulateIons($file) {
+            $buffer = NULL;
+
+            foreach(\FileSystem::IteratesAllLines($file) as $line) {
+                if ($line == MgfParser::BeginIons) {
+                    $buffer = [$line];
+                } else if ($line = MgfParser::EndIons) {
+                    # Push the last line
+                    array_push($buffer, $line);
+                    # This is a new ion
+                    # Parse this new ion data.
+                    yield self::ParseIon($buffer);
+                } else {
+                    array_push($buffer, $line);
+                }
+            }
+        }
+
+        /**
+         * @param string|string[] $buffer Data text content or text lines.
+        */
+        public static function ParseIon($buffer) {
+            if (is_string($buffer)) {
+                $buffer = \StringHelpers::LineTokens($buffer);
+            }
+
+            $mzInto = [];
+            $header = [];
+            $i      = 0;
+            $line   = "";
+            $name   = "";
+            $value  = "";
+            
+            # i = 1 for skip the first line: BEGIN IONS
+            for($i = 1; $i < count($buffer); $i++) {
+                $line = $buffer[$i];
+
+                if (\Strings::InStr($line, "=") > 0) {
+                    $tuple              = \StringHelpers::GetTagValue($line, "=");
+                    list($name, $value) = \Utils::Tuple($tuple);
+                    
+                    if ($name == "PEPMASS") {
+                        $header[$name] = explode(" ", $value)[0];
+                    } else {
+                        $header[$name] = $value;
+                    }
+                }
+            }
+
+            # skip the last line: END IONS
+            for ($i = 1; $i < count($buffer) - 1; $i++) {
+
+            }
+        }
     }
 }
--- BioDeep/IO/mzXML/Scan.php
+++ BioDeep/IO/mzXML/Scan.php
@@ -2,6 +2,8 @@
 
 namespace BioDeep\IO {
 
+    # XML file models
+
     class scan {
     
         #region "property"
