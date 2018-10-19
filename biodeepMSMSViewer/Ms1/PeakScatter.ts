6603beda955fa9c24377b47d93ae67959ac7c6cc
xieguigang <xie.guigang@gcmodeller.org>
2018-10-20 02:08:47 +0800

    ms1 peakscatter?

---
+++ biodeepMSMSViewer/Ms1/PeakScatter.ts
@@ -0,0 +1,21 @@
+﻿namespace BioDeep.MSMSViewer.PeakScatter {
+
+    export class PlotRenderer {
+
+        public margin: Canvas.Margin;
+        public size: Canvas.Size;
+
+        public constructor(
+            size: Canvas.Size,
+            margin: Canvas.Margin = <Canvas.Margin>{
+                top: 20, right: 20, bottom: 30, left: 40
+            }) {
+
+            this.margin = Canvas.Margin.Object(margin);
+            this.size = <Canvas.Size>{
+                width: size.width - this.margin.horizontal,
+                height: size.height - this.margin.vertical
+            };
+        }
+    }
+}
