c9a618c013425c289fac88004920eede61edb900
xieguigang <xie.guigang@gcmodeller.org>
2019-07-03 18:12:28 +0800

    populate mgf ion source data

--- BioDeep/IO/Convertor.php
+++ BioDeep/IO/Convertor.php
@@ -21,7 +21,6 @@ class Convertor {
         if (!file_exists($mzXML)) {
             return false;
         } else {
-            $raw = new \BioDeep\IO\mzXML($mzXML);
             $dir = dirname($mgf);
 
             if (!file_exists($dir)) {
@@ -29,27 +28,48 @@ class Convertor {
             }
         }
 
-        using(new \StreamWriter($mgf, false), function(\StreamWriter $writer) use ($raw) {
-            foreach($raw->yieldAllMs2() as $ms2) {
-                if ($ms2->peaks->PeaksLength() == 0) {
-                    # 没有碎片峰。。。
-                    continue;
-                } else {
-                    $mz     = $ms2->precursorMz->data;
-                    $rt     = $ms2->scan->rt();
-                    $title  = self::scanTitle($raw->fileName, $ms2);
-                    $charge = $ms2->precursorMz->precursorCharge;
-                    $meta   = new PrecursorIon(
-                        $mz, $rt, 
-                        $ms2->precursorMz->precursorIntensity, 
-                        $charge, $title
-                    );
-                    $ion = MgfWriter::CreateDocument($meta, $ms2->peaks->data);
-    
-                    $writer->WriteLine($ion);
-                }
+        using(new \StreamWriter($mgf, false), function(\StreamWriter $writer) use ($mzXML) {
+            foreach(self::mzXML2MgfTextBlocks($mzXML) as $ion) {
+                $writer->WriteLine($ion);
             }
         });
+    }
+
+    /** 
+     * Convert all of the ions in mzXML raw file as the mgf ASCII ion text
+     * 
+     * @param string|\BioDeep\IO\mzXML If the value of this parameter is string, 
+     *            then it should be a valid file path of the mzXML data file, 
+     *            else it should be a ``\BioDeep\IO\mzXML`` data model.
+    */
+    public static function mzXML2MgfTextBlocks($mzXML) {
+        if (is_string($mzXML) && !file_exists($mzXML)) {
+            return false;
+        } else if (is_string($mzXML)) {
+            $raw = new \BioDeep\IO\mzXML($mzXML);
+        } else {
+            $raw = $mzXML;
+        }
+
+        foreach($raw->yieldAllMs2() as $ms2) {
+            if ($ms2->peaks->PeaksLength() == 0) {
+                # 没有碎片峰。。。
+                continue;
+            } else {
+                $mz     = $ms2->precursorMz->data;
+                $rt     = $ms2->scan->rt();
+                $title  = self::scanTitle($raw->fileName, $ms2);
+                $charge = $ms2->precursorMz->precursorCharge;
+                $meta   = new PrecursorIon(
+                    $mz, $rt, 
+                    $ms2->precursorMz->precursorIntensity, 
+                    $charge, $title
+                );
+                $ion = MgfWriter::CreateDocument($meta, $ms2->peaks->data);
+
+                yield $ion;
+            }
+        }
     }
 
     private static function scanTitle(string $fileName, ScanReader $ms2) {
---
+++ BioDeep_mz-Browser/app/index.php
@@ -0,0 +1,12 @@
+<?php
+
+include __DIR__ . "/../../../dotnet/package.php";
+include __DIR__ . "/../../autoload.php";
+
+$mzXML = __DIR__ . "/../../test/GABA.mzXML";
+$raw = new \BioDeep\IO\mzXML($mzXML);
+
+foreach(\BioDeep\IO\Convertor::mzXML2MgfTextBlocks($raw) as $ion) {
+    echo $ion;
+    echo "\n";
+}
