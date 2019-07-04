5794b20a329545c63c6041bc0b8aa0fce0169685
xieguigang <xie.guigang@gcmodeller.org>
2019-07-04 14:15:40 +0800

    add TIC model

---
+++ BioDeep_mz-web/Models/ChromatogramTick.ts
@@ -0,0 +1,16 @@
+﻿namespace BioDeep.Models {
+
+    export interface ChromatogramTick {
+        rt: number;
+        intensity: number;
+    }
+
+    export function TIC(ions: IEnumerator<BioDeep.IO.mgf>): IEnumerator<ChromatogramTick> {
+        return ions
+            .OrderBy(i => i.rt)
+            .Select(i => <ChromatogramTick>{
+                rt: i.rt,
+                intensity: i.intensity
+            });
+    }
+}
