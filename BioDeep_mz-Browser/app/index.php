9e332f212062b671d0a9b21c672bcfd18798f756
xieguigang <xie.guigang@gcmodeller.org>
2019-07-05 10:32:20 +0800

    add a QE file test

--- BioDeep_mz-Browser/app/index.php
+++ BioDeep_mz-Browser/app/index.php
@@ -3,7 +3,8 @@
 include __DIR__ . "/../../../dotnet/package.php";
 include __DIR__ . "/../../autoload.php";
 
-$mzXML = __DIR__ . "/../../test/GABA.mzXML";
+# $mzXML = __DIR__ . "/../../test/GABA.mzXML";
+$mzXML = __DIR__ . "/B10.mzXML";
 $raw = new \BioDeep\IO\mzXML($mzXML);
 
 foreach(\BioDeep\IO\Convertor::mzXML2MgfTextBlocks($raw) as $ion) {
