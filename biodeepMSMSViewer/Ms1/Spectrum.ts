7689ae1841850713e22bbbf8882d64ef196f956a
xieguigang <xie.guigang@gcmodeller.org>
2019-07-10 14:07:39 +0800

    Update Spectrum.ts
    union ms1 and ms2 fragment in Spectrum chart?

--- biodeepMSMSViewer/Ms1/Spectrum.ts
+++ biodeepMSMSViewer/Ms1/Spectrum.ts
@@ -2,5 +2,15 @@
 
     export class Spectrum extends SvgChart {
 
+        public constructor(
+            size: Canvas.Size | number[] = [800, 500],
+            margin: Canvas.Margin = renderingWork.defaultMargin()) {
+
+            super(size, margin);
+        }
+
+        public renderChart(id: string, ions: IEnumerator<Models.mzInto>) {
+
+        }
     }
 }
